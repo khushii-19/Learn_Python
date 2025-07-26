5974b2a29f52097bee15292e03fc7c3947a8bbb9
Khushi Gupta <khushi97000@gmail.com>
2025-07-26 23:19:34 +0530

    some new topics

--- Chapter 12/types.py
+++ Chapter 12/types.py
@@ -1,4 +1,5 @@
-from typing import List, Union, Tuple 
+# from typing import List, Union, Tuple 
+#by exporting it we can use it directly by passing list like n : list = 
 
 n : int = 5
 
@@ -7,3 +8,6 @@ name: str = "Harry"
 
 def  sum(a: int, b: int) -> int:
     return a+b
+
+a = sum(3,4)
+print(a)
